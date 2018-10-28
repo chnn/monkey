f44738a39407b97ae3bf503a6240e39c191f6c01
Christopher Henn <chris@chrishenn.net>
2018-10-27 17:40:38 -0700

    Add Lexer (Chapter 1)

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate monkey;
+
+use monkey::{Lexer, Token};
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    loop {
+        print!(">>> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).unwrap();
+
+        let mut lexer = Lexer::new(input);
+        let mut token = lexer.next_token();
+
+        while token != Token::EOF {
+            println!("{:?}", token);
+            token = lexer.next_token();
+        }
+    }
+}
